927741ebda67ba2da9b983677e949a803a729045
DhruvGaur32 <154977139+DhruvGaur32@users.noreply.github.com>
2025-07-10 01:33:31 +0530

    Debugging added

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import BoardPage from './pages/BoardPage';
 import './App.css'; 
 function App() {
   const [token, setToken] = useState(localStorage.getItem('token'));
-
+  console.log("REACT_APP_API_URL:", process.env.REACT_APP_API_URL);
   return (
     <Router>
       <Routes>
